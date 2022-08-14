e147b7b6de66d7de58350891f9233f5d937c9d4d
Ji-Hun Kim <jihuun.k@gmail.com>
2022-08-14 23:27:32 +0900

    modify style by cargo fmt
    Signed-off-by: Ji-Hun Kim <jihuun.k@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,21 @@
-use std::env;
-use std::io::Read;
 use select::document::Document;
 use select::predicate::{Attr, Class, Name, Predicate};
+use std::env;
+use std::io::Read;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let input_word = &args[1];
     let mut request_url = String::from("http://small.dic.daum.net/search.do?q=");
     request_url.push_str(input_word);
-    
+
     let body: String = reqwest::blocking::get(&request_url)
         .unwrap()
         .text()
         .unwrap();
 
     let document = Document::from(body.as_str());
-    for node in document
-        .find(Class("search_box")
-        .descendant(Class("cleanword_type"))) {
-        
+    for node in document.find(Class("search_box").descendant(Class("cleanword_type"))) {
         let title = node.find(Class("txt_emph1")).next().unwrap().text();
         println!("[ {} ]", title);
         /* TODO: save the means to some datatype */
